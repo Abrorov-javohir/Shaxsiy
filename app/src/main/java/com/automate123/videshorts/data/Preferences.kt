1bb35abb8f3649dd7a86ae92b566e117ae6367f7
Vlad <vladkalyuzhnyu@gmail.com>
2022-05-08 07:05:46 +0300

    settings

---
+++ app/src/main/java/com/automate123/videshorts/data/Preferences.kt
@@ -0,0 +1,18 @@
+package com.automate123.videshorts.data
+
+import android.content.Context
+import androidx.annotation.Keep
+import dagger.hilt.android.qualifiers.ApplicationContext
+import hu.autsoft.krate.SimpleKrate
+import hu.autsoft.krate.default.withDefault
+import hu.autsoft.krate.stringPref
+import javax.inject.Inject
+
+@Keep
+class Preferences @Inject constructor(@ApplicationContext context: Context) : SimpleKrate(context) {
+
+    var duration by stringPref("duration").withDefault("2")
+
+    val durationMillis: Long
+        get() = duration.toInt() * 1000L
+}
