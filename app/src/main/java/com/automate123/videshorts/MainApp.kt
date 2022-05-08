4d2b9ec092e0fe586ad8580986869a9e3c22c4f9
Vlad <vladkalyuzhnyu@gmail.com>
2022-05-08 10:12:03 +0300

    coil

--- app/src/main/java/com/automate123/videshorts/MainApp.kt
+++ app/src/main/java/com/automate123/videshorts/MainApp.kt
@@ -5,13 +5,17 @@ import androidx.camera.camera2.Camera2Config
 import androidx.camera.core.CameraXConfig
 import androidx.hilt.work.HiltWorkerFactory
 import androidx.work.Configuration
+import coil.ImageLoader
+import coil.ImageLoaderFactory
+import coil.memory.MemoryCache
+import coil.request.CachePolicy
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 import javax.inject.Inject
 
 @HiltAndroidApp
 @Suppress("unused")
-class MainApp : Application(), CameraXConfig.Provider, Configuration.Provider {
+class MainApp : Application(), CameraXConfig.Provider, ImageLoaderFactory, Configuration.Provider {
 
     @Inject
     lateinit var workerFactory: HiltWorkerFactory
@@ -27,6 +31,18 @@ class MainApp : Application(), CameraXConfig.Provider, Configuration.Provider {
         return CameraXConfig.Builder
             .fromConfig(Camera2Config.defaultConfig())
             .build();
+    }
+
+    override fun newImageLoader(): ImageLoader {
+        return ImageLoader.Builder(applicationContext)
+            .allowRgb565(true)
+            .memoryCache {
+                MemoryCache.Builder(applicationContext)
+                    .maxSizePercent(0.3)
+                    .build()
+            }
+            .diskCachePolicy(CachePolicy.DISABLED)
+            .build()
     }
 
     override fun getWorkManagerConfiguration(): Configuration {
